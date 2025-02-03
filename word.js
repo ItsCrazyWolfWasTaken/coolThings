6a4764749ce03de0b79d24fee2677a3a132eec53
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2025-02-03 11:00:29 -0500

    Update word.js

--- word.js
+++ word.js
@@ -17,9 +17,9 @@ fetch("words.json")
 
         if (wordData) {
             document.getElementById("word").textContent = word;
-            document.getElementById("pos").textContent = data[word].part_of_speech;
-            document.getElementById("definition").textContent = data[word].definition;
-            document.getElementById("sentence").textContent = data[word].used_in_sentence;
+            document.getElementById("pos").textContent = wordData.part_of_speech;
+            document.getElementById("definition").textContent = wordData.definition;
+            document.getElementById("sentence").textContent = wordData.used_in_sentence;
         } else {
             document.body.innerHTML = "<h1>Word Not Found</h1><a href='index.html'>Back</a>";
         }
