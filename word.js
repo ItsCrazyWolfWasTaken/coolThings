e128a39172b1f24de4b47c4c2eb538d345b57526
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2025-02-03 12:19:37 -0500

    ukglsdufhs

--- word.js
+++ word.js
@@ -2,7 +2,6 @@ const params = new URLSearchParams(window.location.search);
 const word = params.get("word");
 
 fetch("master.json")
-print("pass")
     .then(response => response.json())
     .then(data => {
         let wordData = null;
